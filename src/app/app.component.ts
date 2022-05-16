8bfff4463bba9bd7b30ce5f6a36654aa88cd314f
YS_workPC <ong_ys86@yahoo.com.sg>
2022-05-16 18:29:13 +0800

    add copy function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   billForm !: FormGroup;
   totalBill = 0;
   individualBill = 0;
-  IsSubmitted = false;
+  isSubmitted = false;
 
 
   constructor(private fb : FormBuilder){}
@@ -39,7 +39,7 @@ export class AppComponent implements OnInit {
     const friend = this.fb.group({
       friendName : ["",Validators.required],
       paidAmt : [0,Validators.required],
-      comment : ""
+      comment : [""]
     })
     this.friends.push(friend)
   }
@@ -48,8 +48,11 @@ export class AppComponent implements OnInit {
     this.friends.removeAt(i);
   }
 
+  submitTrue(){
+    this.isSubmitted = true
+  }
+
   onSubmit(){
-    this.IsSubmitted=true
     this.totalBill = 0
     this.individualBill = 0
     console.log(this.billForm.value)
@@ -57,9 +60,61 @@ export class AppComponent implements OnInit {
       this.totalBill = this.totalBill + e["paidAmt"]
     });
     this.individualBill = this.totalBill / this.billForm.value["friends"].length
-    console.log("total bill = %f",this.totalBill)
-    console.log("individual bill = %f", this.individualBill)
+  }
+
+  onCopy(){
+    this.onSubmit()
+    let header = "Bill for " + this.billForm.value["place"]
+    let paymentPart1 = ""
+    let paymentPart2 = "Bill = "
+    let paymentPart3 = "= $" + this.totalBill
+    let paymentPart4 = ""
+    this.billForm.value["friends"].forEach( (e:{friendName:string,paidAmt:number,comment:string}) =>{
+
+      if (e["paidAmt"] > 0 || e["paidAmt"] < 0){
+        paymentPart1 = paymentPart1 + e["friendName"] +" paid $" + e["paidAmt"] + " (" + e["comment"] + ")" +"\n"
+        paymentPart2 = paymentPart2 + "+ $" + e["paidAmt"] + " "
+
+        paymentPart4 = paymentPart4
+                      + e["friendName"]
+                      + " bill = $" + this.roundNumber(this.individualBill)
+                      + " - $" + this.roundNumber(e["paidAmt"])
+                      + " = $" + this.roundNumber(this.individualBill - e["paidAmt"])
+                      + "\n"
+      }
+      else {
+        paymentPart4 = paymentPart4
+        + e["friendName"]
+        + " bill = $" + this.roundNumber(this.individualBill)
+        + "\n"
+      }
+
+
+    })
+
+    console.log(header)
+    console.log(paymentPart1)
+    console.log(paymentPart2)
+    console.log(paymentPart3)
+    console.log(paymentPart4)
+
+    const copyString = header + "\n\n"
+                      + paymentPart1
+                      + "\n" + paymentPart2
+                      + "\n" + paymentPart3 + "\n"
+                      + "\n" + paymentPart4
+
+    navigator.clipboard.writeText(copyString)
+      .then( (sucess) =>{
+        console.log("copy successfully");
+      },
+      (error) =>{
+        console.log("copy failed");
+      });
   }
 
 
+  private roundNumber(num: number){
+    return Math.round((num + Number.EPSILON) * 100) / 100
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { MaterialModule } from './material.module';
     HeaderComponent
   ],
   imports: [
+    FlexLayoutModule,
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
